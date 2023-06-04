4f3e3f6ba8461158da9b91aac10e33e7803b4818
Magda <>
2023-06-04 14:16:05 -0400

    fix

--- server.js
+++ server.js
@@ -48,9 +48,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/comments', commentsRoute);
 
+
 if (process.env.NODE_ENV === 'production') {
 	// Serve any static files
-	app.use(express.stat//ic(path.join(__dirname, 'frontend/build')));
+	app.use(express.static(path.join(__dirname, 'frontend/build')));
 	// Handle React routing, return all requests to React app
 	app.get('*', function(req, res) {
 		res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
